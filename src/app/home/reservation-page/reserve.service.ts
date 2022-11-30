4e0c204b33a32d9711f9652cd191dd23210a276e
KankichiJibiki <kan.0408@outlook.jp>
2022-11-30 20:57:54 +0900

    resolve conflict

--- src/app/home/reservation-page/reserve.service.ts
+++ src/app/home/reservation-page/reserve.service.ts
@@ -25,7 +25,7 @@ export class ReserveService implements OnInit{
   timeForDisplay :string = "";
 
   //For ajax
-    private base_url: string = 'http://localhost/tomoe_db_restapi/';
+    private base_url: string = 'http://tomoereservation-env.eba-9z6xx2ex.ap-northeast-1.elasticbeanstalk.com/tomoe_db_restapi_prod/';
   //
 
   bookingInfo: Booking = {

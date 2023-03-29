fc31768a197f036677e9f0734c562ffe7eec9e58
KankichiJibiki <kan.0408@outlook.jp>
2023-03-29 21:07:54 +0900

    done with restricted process and buildspec file added

--- src/app/constants/ApiUrls.ts
+++ src/app/constants/ApiUrls.ts
@@ -2,6 +2,7 @@ export class ApiUrls {
     //* Reservation CRUD api
     static readonly RESERVATION_URL: string = "/Reservation";
         static readonly     RESERVATION_ACTION_RESERVE: string = "/Reserve";
+        static readonly     RESERVATION_ACTION_RESTRICTED: string = "/GetRestrictedDates";
 
     //* Instagram api
     static readonly INSTAGRAM_URL: string = "/Instagram";
---
+++ src/app/model/restrictedDates.ts
@@ -0,0 +1,4 @@
+export class RestrictedDates
+{
+    restrictedDates?: Date;
+}

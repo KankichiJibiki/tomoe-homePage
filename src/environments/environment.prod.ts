ca7b5d926fbd0279ddf63315d46ea679a02bb46b
KankichiJibiki <kan.0408@outlook.jp>
2023-05-13 15:51:22 +0900

    Aws lambda endpoint has changed to an uri that is from api gateway

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://nas4nf4muiugv3ead5lawd2n4q0zvolx.lambda-url.ap-northeast-1.on.aws/api",
+  apiUrl: "https://lijscr7j81.execute-api.ap-northeast-1.amazonaws.com/api",
 };

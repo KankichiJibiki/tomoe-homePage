a47960eec91d7bb5f2da27afba02c8b927051e93
KankichiJibiki <kan.0408@outlook.jp>
2023-08-05 17:41:05 +0900

    multiple images allowed

--- src/app/interfaces/excludedMenuInterface.ts
+++ src/app/interfaces/excludedMenuInterface.ts
@@ -3,7 +3,7 @@ export interface ExcludedMenuInterface{
     name_en? : String,
     price_jp? : String,
     price_en? : String,
-    image? : String,
+    image? : String[],
     discription_jp? : String,
     discription_en? : String,
     start? : Date,
--- src/app/model/excludedMenus.ts
+++ src/app/model/excludedMenus.ts
@@ -6,7 +6,10 @@ export class ExcludedDishs{
         name_en: "Summer Course【涼風 - Suzukaze】",
         price_jp: "20,000(税込)",
         price_en: "20,000(Including tax)",
-        image: "../../../assets/images/obon-course/course_menu.JPG",
+        image: [
+            "../../../assets/images/obon-course/course_menu.JPG",
+            "../../../assets/images/obon-course/IMG_2794.JPG",
+        ],
         discription_jp: "8/11(金) ～ 8/16(水) 期間限定のお盆コースです。\n 前菜からデザートまで夏の旬物からなる献立となります。\n 和食巴の真剣な日本料理をご提供させていただきます。",
         discription_en: "Limited Summer Course, 8/11(Fry) to 8/16(Wed)\n Dishs that contain all summer ingredients served.\n Do not miss out!",
         start : new Date("2023-08-03"),
